ae1a3cd1a986c0b9ced77ff91bb2e67c3c90b556
richard <rick.billeci@gmail.com>
2025-07-23 20:34:04 +0200

    #27 Remove use of session variables

--- boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_topics_create.sql
+++ boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_topics_create.sql
@@ -3,8 +3,10 @@ CREATE PROCEDURE sp_topics_create(
     IN p_name VARCHAR(255),
     IN p_partitions INT)
 BEGIN
+    DECLARE v_id BIGINT;
+
     INSERT INTO topics (tenant, name, partitions) VALUES (p_tenant, p_name, p_partitions);
-    SET @topic_id = LAST_INSERT_ID();
+    SET v_id = LAST_INSERT_ID();
 
     -- INSERT THE PARTITIONS
     INSERT INTO partitions (tenant_name, topic_id, topic_name, partition_number, partitions)
@@ -12,7 +14,7 @@ BEGIN
         SELECT 0
         UNION ALL
         SELECT n + 1 FROM numbers WHERE n < p_partitions - 1)
-    SELECT p_tenant, @topic_id, p_name, n, p_partitions FROM numbers;
+    SELECT p_tenant, v_id, p_name, n, p_partitions FROM numbers;
     -- RETURN THE TOPIC ID
-    SELECT @topic_id AS id;
+    SELECT v_id AS id;
 END;
--- boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_topics_subscribe.sql
+++ boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_topics_subscribe.sql
@@ -2,10 +2,11 @@ CREATE PROCEDURE sp_topics_subscribe(
     IN p_consumer_group_id BIGINT,
     IN p_topic_id BIGINT)
 BEGIN
+    DECLARE v_id BIGINT;
     INSERT INTO subscriptions(consumer_group_id, topic_id) VALUES (p_consumer_group_id, p_topic_id);
-    SET @sub_id = LAST_INSERT_ID();
+    SET v_id = LAST_INSERT_ID();
     INSERT INTO subscription_offsets(subscription_id, partition_id, committed_offset)
-        SELECT @sub_id, id, 0 FROM partitions WHERE topic_id = p_topic_id;
-    SELECT @sub_id AS id;
+        SELECT v_id, id, 0 FROM partitions WHERE topic_id = p_topic_id;
+    SELECT v_id AS id;
 END;
 
