81868e97b0b8be2f84d6673b1a75d74ae80aee36
richard <rick.billeci@gmail.com>
2025-07-23 17:54:59 +0200

    #27 Fix HWM update

--- boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_events_publish_multi.sql
+++ boxy-persistence/src/main/resources/db/changelog/mysql/sp/sp_events_publish_multi.sql
@@ -5,6 +5,6 @@ BEGIN
     INSERT INTO events(partition_id, data)
         SELECT p_partition_id, jt.data
         FROM JSON_TABLE(p_events, '$[*]' COLUMNS(data JSON PATH '$')) AS jt;
-    UPDATE partitions SET high_watermark = LAST_INSERT_ID() WHERE id = p_partition_id;
+    UPDATE partitions SET high_watermark = GREATEST(high_watermark, LAST_INSERT_ID()) WHERE id = p_partition_id;
 END;
 
