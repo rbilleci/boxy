1ac338bf6e8d7407df14e6fbdc6d291f7925bb62
richard <rick.billeci@gmail.com>
2025-07-27 19:55:37 +0200

    #4 Readability

--- boxy-db/src/main/resources/db/changelog/mysql/sp/sp_workers_check_in__heartbeat.sql
+++ boxy-db/src/main/resources/db/changelog/mysql/sp/sp_workers_check_in__heartbeat.sql
@@ -10,16 +10,25 @@ BEGIN
     DECLARE EXIT HANDLER FOR SQLEXCEPTION BEGIN RESIGNAL; END;
 
     INSERT INTO workers (
-        node_id, consumer_group_id, weight, last_heartbeat, heartbeat_interval, heartbeat_deadline)
+        node_id,
+        consumer_group_id,
+        weight,
+        last_heartbeat,
+        heartbeat_interval,
+        heartbeat_deadline)
     VALUES (
-        p_node_id, p_consumer_group_id, p_weight, CURRENT_TIMESTAMP(3), p_heartbeat_interval, p_heartbeat_deadline)
+        p_node_id,
+        p_consumer_group_id,
+        p_weight,
+        CURRENT_TIMESTAMP(3),
+        p_heartbeat_interval,
+        p_heartbeat_deadline)
     ON DUPLICATE KEY UPDATE
-        id = LAST_INSERT_ID(id),
-        consumer_group_id = VALUES(consumer_group_id),
-        weight = VALUES(weight),
-        last_heartbeat = VALUES(last_heartbeat),
-        heartbeat_interval = VALUES(heartbeat_interval),
-        heartbeat_deadline = VALUES(heartbeat_deadline);
+        id                  = LAST_INSERT_ID(id),
+        weight              = VALUES(weight),
+        last_heartbeat      = VALUES(last_heartbeat),
+        heartbeat_interval  = VALUES(heartbeat_interval),
+        heartbeat_deadline  = VALUES(heartbeat_deadline);
 
     SET p_worker_id = LAST_INSERT_ID();
 END;
