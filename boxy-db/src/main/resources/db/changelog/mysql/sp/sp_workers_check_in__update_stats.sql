a55f7a543cfc7016c356b510b2af0b54841c8488
richard <rick.billeci@gmail.com>
2025-07-28 20:21:09 +0200

    Fix documentation

--- boxy-db/src/main/resources/db/changelog/mysql/sp/sp_workers_check_in__update_stats.sql
+++ boxy-db/src/main/resources/db/changelog/mysql/sp/sp_workers_check_in__update_stats.sql
@@ -30,8 +30,8 @@ BEGIN
      WHERE id = p_consumer_group_id;
 
     -- Calculate adaptive heartbeat interval based on heartbeat_interval_default
-    -- Use the configurable heartbeat_target_qps instead of hardcoded 10.0
-    -- This controls the cluster-wide check-ins per second
+    -- Use the configurable heartbeat_target_qps
+    -- This controls the ideal cluster-wide check-ins per second
     SET p_heartbeat_interval = GREATEST(p_heartbeat_interval, p_active_workers / @target_qps);
     
     -- Apply min and max constraints to the heartbeat interval
