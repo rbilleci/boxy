8e13384dec68c9c4bb7660d1cc137de502f7b818
richard <rick.billeci@gmail.com>
2025-08-04 21:28:27 +0200

    #65 removed `version` since it is currently unused

--- boxy-core/src/main/java/boxy/core/domain/Lease.java
+++ boxy-core/src/main/java/boxy/core/domain/Lease.java
@@ -5,7 +5,6 @@ import java.time.Instant;
 public record Lease(
         long cursorId,
         String consumerId,
-        long version,
         Instant acquiredAt,
         Instant releasedAt,
         Instant releaseDeadline,
