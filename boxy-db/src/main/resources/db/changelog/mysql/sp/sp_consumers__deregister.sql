df8f63b2c5761b383dbe0ebf03f7d5de9b05b43a
R Billeci <rbilleci@users.noreply.github.com>
2025-11-24 00:04:45 +0100

    Delete leases when deregistering consumers

--- boxy-db/src/main/resources/db/changelog/mysql/sp/sp_consumers__deregister.sql
+++ boxy-db/src/main/resources/db/changelog/mysql/sp/sp_consumers__deregister.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE sp_consumers__deregister(IN p_consumer_id VARCHAR(36))
 BEGIN
     DECLARE EXIT HANDLER FOR SQLEXCEPTION BEGIN ROLLBACK; END;
     START TRANSACTION;
-        UPDATE leases SET consumer_id = NULL, locked_until = NULL WHERE consumer_id = p_consumer_id;
+        DELETE FROM leases WHERE consumer_id = p_consumer_id;
         DELETE FROM consumers WHERE id = p_consumer_id;
     COMMIT;
 END;
